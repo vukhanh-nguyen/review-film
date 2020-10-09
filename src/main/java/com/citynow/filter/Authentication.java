a6f5c654c84a7e932265934286adb371105707d3
Vu Khanh <vukhanh.nguyen@citynow.vn>
2020-10-09 16:48:59 +0700

    Add filter

---
+++ src/main/java/com/citynow/filter/Authentication.java
@@ -0,0 +1,51 @@
+package com.citynow.filter;
+
+
+import com.citynow.model.UserModel;
+import com.citynow.utils.SessionUtil;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter({"/*"})
+public class Authentication implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String url = request.getRequestURI();
+        UserModel model = (UserModel) SessionUtil.getInstance().getValue(request, "USERMODEL");
+        if (url.startsWith("/admin")) {
+            if (model != null) {
+                if (model.getRole().getCode().equals("ADMIN")) {
+                    chain.doFilter(servletRequest, servletResponse);
+                } else if (model.getRole().getCode().equals("USER")) {
+                    response.sendRedirect(request.getContextPath()+"/home");
+                }
+            } else {
+                response.sendRedirect(request.getContextPath()+"/login");
+            }
+        } else if (url.startsWith("/create-post")) {
+            if (model != null){
+                chain.doFilter(servletRequest, servletResponse);
+            }else{
+                response.sendRedirect(request.getContextPath()+"/login");
+            }
+        } else {
+            chain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
